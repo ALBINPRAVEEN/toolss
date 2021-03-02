1ebec1fa9d8b201c37d83c8cc5925c766ffea57a
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2021-03-02 12:56:18 +0530

    Update Toolss.py

--- Toolss.py
+++ Toolss.py
@@ -11,9 +11,8 @@ def menu():
 ....##....##.....##.##.....##.##.......##....##.##....##
 ....##.....#######...#######..########..######...######.
 ========================================
-Created By AnonHacker
-Channel: youtube.com/kalinuxx
-Facebook: facebook.com/kalinuxtutorials
+Created By ALBY
+
 Ver: 2.0
 ----
 ONLY FOR TERMUX!
